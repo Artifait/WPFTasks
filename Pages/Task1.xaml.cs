7dd5ff02724a55547eff7e3ce8e627d089122cfb
Artur <am3044288gmail.com>
2024-11-06 12:27:44 +0700

    Startuem

--- Pages/Task1.xaml.cs
+++ Pages/Task1.xaml.cs
@@ -1,126 +1,15 @@
-﻿using System;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows;
+﻿
 using System.Windows.Controls;
+using WPFTasks.ViewModels;
 
 namespace WPFTasks.Pages
 {
     public partial class Task1 : Page
     {
-        private CancellationTokenSource _numberTokenSource;
-        private CancellationTokenSource _letterTokenSource;
-        private CancellationTokenSource _symbolTokenSource;
-        private readonly Random _random = new Random();
-        private readonly int _delay = 500; // Задержка между итерациями в миллисекундах
-
         public Task1()
         {
             InitializeComponent();
-
-            CheckNumber.Checked += (s, e) => StartNumberGenerator();
-            CheckNumber.Unchecked += (s, e) => StopNumberGenerator();
-
-            CheckLetter.Checked += (s, e) => StartLetterGenerator();
-            CheckLetter.Unchecked += (s, e) => StopLetterGenerator();
-
-            CheckSymbol.Checked += (s, e) => StartSymbolGenerator();
-            CheckSymbol.Unchecked += (s, e) => StopSymbolGenerator();
-        }
-
-        private async void StartNumberGenerator()
-        {
-            if (_numberTokenSource != null) return;
-
-            _numberTokenSource = new CancellationTokenSource();
-            var token = _numberTokenSource.Token;
-
-            try
-            {
-                await Task.Run(async () =>
-                {
-                    while (!token.IsCancellationRequested)
-                    {
-                        await Dispatcher.InvokeAsync(() => AddCharacter('N'));
-                        await Task.Delay(_delay, token);
-                    }
-                }, token);
-            }
-            catch (TaskCanceledException) {  }
-        }
-
-        private void StopNumberGenerator()
-        {
-            _numberTokenSource?.Cancel();
-            _numberTokenSource = null;
-        }
-
-        private async void StartLetterGenerator()
-        {
-            if (_letterTokenSource != null) return;
-
-            _letterTokenSource = new CancellationTokenSource();
-            var token = _letterTokenSource.Token;
-
-            try
-            {
-                await Task.Run(async () =>
-                {
-                    while (!token.IsCancellationRequested)
-                    {
-                        await Dispatcher.InvokeAsync(() => AddCharacter('L'));
-                        await Task.Delay(_delay, token);
-                    }
-                }, token);
-            }
-            catch (TaskCanceledException) { }
-        }
-
-        private void StopLetterGenerator()
-        {
-            _letterTokenSource?.Cancel();
-            _letterTokenSource = null;
-        }
-
-        private async void StartSymbolGenerator()
-        {
-            if (_symbolTokenSource != null) return;
-
-            _symbolTokenSource = new CancellationTokenSource();
-            var token = _symbolTokenSource.Token;
-
-            try
-            {
-                await Task.Run(async () =>
-                {
-                    while (!token.IsCancellationRequested)
-                    {
-                        await Dispatcher.InvokeAsync(() => AddCharacter('S'));
-                        await Task.Delay(_delay, token);
-                    }
-                }, token);
-            }
-            catch (TaskCanceledException) { }
-        }
-
-        private void StopSymbolGenerator()
-        {
-            _symbolTokenSource?.Cancel();
-            _symbolTokenSource = null;
-        }
-
-        private void AddCharacter(char type)
-        {
-            char character = type switch
-            {
-                'N' => (char)('0' + _random.Next(10)), 
-                'L' => (char)('A' + _random.Next(26)),
-                'S' => new[] { '!', '@', '#', '$', '%', '^', '&', '*', '(', ')' }[_random.Next(10)],
-                _ => throw new ArgumentException("Invalid character type")
-            };
-
-            Output.Text += character;
+            DataContext = new Task1ViewModel();
         }
     }
 }
---
+++ ViewModels/Task1ViewModel.cs
@@ -0,0 +1,119 @@
+﻿
+using System.ComponentModel;
+using System.Text;
+
+namespace WPFTasks.ViewModels
+{
+    public class Task1ViewModel : INotifyPropertyChanged
+    {
+        private bool _isPrimeGeneratorEnabled;
+        private bool _isFibonacciGeneratorEnabled;
+        private string _primeNumbersOutput;
+        private string _fibonacciNumbersOutput;
+        private CancellationTokenSource _cancellationTokenSource;
+
+        public string PrimeNumbersOutput
+        {
+            get => _primeNumbersOutput;
+            set
+            {
+                _primeNumbersOutput = value;
+                OnPropertyChanged(nameof(PrimeNumbersOutput));
+            }
+        }
+
+        public string FibonacciNumbersOutput
+        {
+            get => _fibonacciNumbersOutput;
+            set
+            {
+                _fibonacciNumbersOutput = value;
+                OnPropertyChanged(nameof(FibonacciNumbersOutput));
+            }
+        }
+
+        public bool IsPrimeGeneratorEnabled
+        {
+            get => _isPrimeGeneratorEnabled;
+            set
+            {
+                _isPrimeGeneratorEnabled = value;
+                OnPropertyChanged(nameof(IsPrimeGeneratorEnabled));
+                if (value) StartPrimeGenerator();
+            }
+        }
+
+        public bool IsFibonacciGeneratorEnabled
+        {
+            get => _isFibonacciGeneratorEnabled;
+            set
+            {
+                _isFibonacciGeneratorEnabled = value;
+                OnPropertyChanged(nameof(IsFibonacciGeneratorEnabled));
+                if (value) StartFibonacciGenerator();
+            }
+        }
+
+        public Task1ViewModel()
+        {
+            _cancellationTokenSource = new CancellationTokenSource();
+        }
+
+        private async void StartPrimeGenerator()
+        {
+            await Task.Run(() =>
+            {
+                int num = 2;
+                var sb = new StringBuilder();
+                while (!_cancellationTokenSource.Token.IsCancellationRequested && IsPrimeGeneratorEnabled)
+                {
+                    if (IsPrime(num))
+                    {
+                        sb.AppendLine(num.ToString());
+                        PrimeNumbersOutput = sb.ToString();
+                        Thread.Sleep(500);
+                    }
+                    num++;
+                }
+            });
+        }
+
+        private async void StartFibonacciGenerator()
+        {
+            await Task.Run(() =>
+            {
+                int a = 0, b = 1;
+                var sb = new StringBuilder();
+                while (!_cancellationTokenSource.Token.IsCancellationRequested && IsFibonacciGeneratorEnabled)
+                {
+                    int temp = a;
+                    a = b;
+                    b = temp + b;
+
+                    sb.AppendLine(a.ToString());
+                    FibonacciNumbersOutput = sb.ToString();
+                    Thread.Sleep(500);
+                }
+            });
+        }
+
+        private bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+            if (number <= 3) return true;
+            if (number % 2 == 0 || number % 3 == 0) return false;
+            for (int i = 5; i * i <= number; i += 6)
+            {
+                if (number % i == 0 || number % (i + 2) == 0)
+                    return false;
+            }
+            return true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
